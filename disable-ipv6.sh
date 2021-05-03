e1685122517bb7a2b4143eba5c9abb6eac39fb35
sslim <siewsin@gmail.com>
2021-05-03 17:55:31 +0100

    added disable ipv6 script

---
+++ disable-ipv6.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root" 
+	exit 1
+fi
+
+workdir="$(dirname $(realpath ${BASH_SOURCE[0]}))"
+
+#disable IPv6, gpsd fix
+sudo patch -Nu /etc/sysctl.conf -i "$workdir/stratux_sysctl.patch"
+sudo patch -Nu /lib/systemd/system/gpsd.socket -i "$workdir/stratux_gpsdsocket.patch"
+
+#reboot
+echo "You can reboot"
